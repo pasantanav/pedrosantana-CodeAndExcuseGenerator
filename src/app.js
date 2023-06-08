bd6f369607eb751ddb609e3a8217b64d34b71bf3
Pedro Santana <134099838+pasantanav@users.noreply.github.com>
2023-06-08 01:54:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";*/
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuseHTMLcode =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+  return excuseHTMLcode;
 };
+document.querySelector("#excuse").innerHTML = window.onload();
